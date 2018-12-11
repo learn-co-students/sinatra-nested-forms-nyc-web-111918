6bcc9c23a23bc806281db6f68eaf8f7b564e089c
Damon Collins <damon.c.p.collins@gmail.com>
2018-12-11 17:28:16 -0500

    done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+  @@ships = Array.new
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships = []
+  end
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    @@ships << self
+  end
 end
