4eda4a9b89dfa3537ff7503ca7903877feefe9ff
Tonyjlum <tonyjlum@gmail.com>
2018-12-11 22:02:47 -0500

    the pirates/show

--- app.rb
+++ app.rb
@@ -5,5 +5,26 @@ module FormsLab
 
     # code other routes/actions here
 
+    get "/" do
+      "Welcome to the Nested Forms Lab! \nlet's navigate to the '/new'"
+    end
+
+    get "/new" do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      #make objects here
+      #Pirate.new(params[:pirate])
+      #@pirate = Pirate.all
+      @pirate = Pirate.new(params[:pirate])
+      @ships = params[:pirate][:ships].map do |ship|
+        Ship.new(ship)
+      end
+
+
+      erb :"pirates/show"
+    end
+
   end
 end
